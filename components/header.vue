9b43a3ac8acaea7971e262506b8b2db1c0338aee
Naoki-Hidaka <naoki.hidaka@dena.com>
2024-08-11 21:42:32 +0900

    modify header button color

--- components/header.vue
+++ components/header.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="flex flex-row py-5 px-8 bg-gray-100 items-center border-b border-gray-400">
     <ULink class="text-xl flex-grow" to="/">Todo List</ULink>
-    <UButton class="py-3 px-6 bg-yellow-400 hover:bg-yellow-600 text-black" label="登録"/>
+    <UButton class="py-3 px-6 bg-yellow-500 hover:bg-yellow-600 text-white" label="登録"/>
   </div>
 </template>
 
